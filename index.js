1723a8b27e53daf9170ca210e0eda1c6377950bd
Rohit <rohityadav787898@gmail.com>
2023-06-25 14:18:47 +0545

    all connected

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const indexRouter = require("./routes");
+
+app.use("/",indexRouter);
 
 app.listen(8000, () => {
   console.log("server running on port 8000");
