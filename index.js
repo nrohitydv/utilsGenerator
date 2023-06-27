6396528667d495a7bea7789eee421c374bd4892e
Rohit <rohityadav787898@gmail.com>
2023-06-27 14:31:38 +0545

    first website using html css express

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require('morgan');
+const ejs=require("ejs");
+const cors=require("cors");
 
 const app = express();
 
-app.use(morgan('combined'))
+app.use(morgan('short'))
+app.use(cors());
+
+app.set("view engine","ejs");
+app.set("views","./views");
+
 const indexRouter = require("./routes");
 
-
+app.use("/",indexRouter)
 
 app.listen(8000, () => {
   console.log("server running on port 8000");
