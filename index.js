c876d6bf400e07f9c3e4e9f46e78480ed3e24823
Rohit <rohityadav787898@gmail.com>
2023-06-27 13:55:11 +0545

    middleware learnt

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const morgan = require('morgan')
+
 const app = express();
 
+app.use(morgan('combined'))
 const indexRouter = require("./routes");
 
-app.use("/", indexRouter);
+
 
 app.listen(8000, () => {
   console.log("server running on port 8000");
